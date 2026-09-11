0dc88ea200a01c34fc41eb8f329fb8a65dd8b4e7
noahwneuman <noah.w.neuman@gmail.com>
2026-09-11 17:05:44 -0400

    Full Range Motion and Intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/ExampleIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/ExampleIntake.java
@@ -1,19 +1,29 @@
 package org.firstinspires.ftc.teamcode.examples;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class ExampleIntake {
+    DcMotor intakeL;
+    DcMotor intakeR;
     public ExampleIntake(HardwareMap hardwareMap){
-        DcMotor intakeL;
-        DcMotor intakeR;
-
         intakeL = hardwareMap.get(DcMotor.class, "intakeL");
         intakeL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         intakeR = hardwareMap.get(DcMotor.class, "intakeR");
         intakeR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        //Triangle for intake
+        //Square for reversing the intake
+
+    }
+    public void spin(double power){
+        intakeL.setPower(power);
+        intakeR.setPower(power);
+    }
+    public void update(Gamepad gamepad1){
+        spin((gamepad1.y) ?1: (gamepad1.x) ?-1:0);
     }
 }
